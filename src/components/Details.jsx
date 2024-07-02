e706ba5b5f6fb8abf279e89d67ba2313737cdf81
Andre Barros <andrebarros7201@gmail.com>
2024-07-02 20:04:24 +0100

    display nav on main section

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -3,7 +3,9 @@ import styles from "../styles/details.module.css";
 export default function Details() {
   return (
     <main className={styles["main"]}>
-      <nav></nav>
+      <nav>
+        <a>Table</a>
+      </nav>
       <div></div>
     </main>
   );
