2a13765797f9c8a72f51c829a07a5a66cc7abcc3
Andre Barros <andrebarros7201@gmail.com>
2024-07-04 20:45:03 +0100

    add basic style to nav in details

--- src/components/details/DetailsNav.jsx
+++ src/components/details/DetailsNav.jsx
@@ -1,8 +1,9 @@
+import styles from '../../styles/detailsNav.module.css'
 import { Link } from "react-router-dom";
 
 export default function DetailsNav() {
   return (
-    <nav>
+    <nav className={styles["details-nav"]}>
       <Link to="/">Table</Link>
       <Link to="/chart">Chart</Link>
     </nav>
