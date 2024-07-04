541d404f5b213c70f6cec91f50b0a7f54e9288ec
Andre Barros <andrebarros7201@gmail.com>
2024-07-05 00:48:13 +0100

    display every year and values on table component

--- src/components/details/Table.jsx
+++ src/components/details/Table.jsx
@@ -1,8 +1,29 @@
-export default function Table() {
+import { AppContext } from "../../App.jsx";
+import { useContext, useEffect } from "react";
+import styles from "../../styles/table.module.css";
 
+function TableItem({ item }) {
+  return (
+    <div className={styles["table-item"]}>
+      <p>Year: {item.year}</p>
+      <p>Final Value: {item.value}€</p>
+    </div>
+  );
+}
+
+export default function Table() {
+  const { investment } = useContext(AppContext);
+
+  useEffect(() => {
+    console.log(investment.growth);
+  }, [investment]);
   return (
     <div>
-      <h3>Table</h3>
+      <div className={styles["table"]}>
+        {investment.growth.map((item) => (
+          <TableItem item={item} key={item.year} />
+        ))}
+      </div>
     </div>
   );
 }
