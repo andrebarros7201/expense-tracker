4e33392e27c50540ff16c33e8bfbb24f632fd585
Andre Barros <andrebarros7201@gmail.com>
2024-07-10 18:15:50 +0100

    show annual growth

--- src/components/details/Table.jsx
+++ src/components/details/Table.jsx
@@ -3,7 +3,7 @@ import { useContext, useState } from "react";
 import styles from "../../styles/table.module.css";
 import PropTypes from "prop-types";
 
-function TableItem({ item }) {
+function TableItem({ item, previousItem }) {
   const [detailsOpen, setDetailsOpen] = useState(false);
   const toggleDetails = () => {
     setDetailsOpen(!detailsOpen);
@@ -12,7 +12,16 @@ function TableItem({ item }) {
     <div className={styles["table-item"]}>
       <div className={styles["table-item-head"]}>
         <p>Year: {item.year}</p>
-        <p>Final Value: {item.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} €</p>
+        <p>
+          Final Value: {item.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} €{" "}
+          {previousItem
+            ? "( +" +
+              parseFloat(item.value - previousItem.value)
+                .toFixed(2)
+                .replace(/\B(?=(\d{3})+(?!\d))/g, " ") +
+              " €)"
+            : "(0€)"}
+        </p>
         <button onClick={toggleDetails}>
           {detailsOpen ? "Close" : "Open"} Details
         </button>
@@ -33,17 +42,28 @@ function TableItem({ item }) {
 
 TableItem.propTypes = {
   item: PropTypes.object.isRequired,
+  previousItem: PropTypes.object,
 };
 
 export default function Table() {
   const { investment } = useContext(AppContext);
 
+  let previousItem = null; // Initialize previous item as null
+
   return (
     <div>
       <div className={styles["table"]}>
-        {investment.growth.map((item) => (
-          <TableItem item={item} key={item.year} />
-        ))}
+        {investment.growth.map((item, index) => {
+          const element = (
+            <TableItem
+              item={item}
+              previousItem={previousItem}
+              key={item.year}
+            />
+          );
+          previousItem = item; // Update previous item for the next iteration
+          return element;
+        })}
       </div>
     </div>
   );
