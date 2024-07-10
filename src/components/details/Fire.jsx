74ac35bba03751811df0181eef996ab60741981e
Andre Barros <andrebarros7201@gmail.com>
2024-07-10 12:43:29 +0100

    add fire component and initial layout and style

--- src/components/details/Fire.jsx
+++ src/components/details/Fire.jsx
@@ -1,1 +1,47 @@
-export default function Fire() {}
+import { useContext, useState } from "react";
+import { AppContext } from "../../App.jsx";
+import styles from "../../styles/fire.module.css";
+
+export default function Fire() {
+  const { investment } = useContext(AppContext);
+  const [monthlyExpenses, setMonthlyExpenses] = useState(0);
+
+  const handleMonthlyExpenses = (value) => {
+    setMonthlyExpenses(value);
+  };
+
+  const calculateTimeToRetire = () => {};
+  return (
+    <>
+      <h2>Financial Independence, Retire Early</h2>
+      <section className={styles["section-fire"]}>
+        <div className={styles["left"]}>
+          <h3>From Investments</h3>
+          <p>
+            4% From Investments:{" "}
+            {parseFloat(investment.fireFourPercent)
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+            €
+          </p>
+          <p></p>
+        </div>
+        <div className={styles["right"]}>
+          <h3>Monthly Expenses</h3>
+          <form className={styles["form-fire"]}>
+            <span>
+              <label htmlFor="monthlyExpenses">Monthly Expenses</label>
+              <input
+                type="number"
+                min={0}
+                id={"monthlyExpenses"}
+                onChange={(e) => handleMonthlyExpenses(e.target.value)}
+              />
+            </span>
+            <button type={"submit"}>Calculate</button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
