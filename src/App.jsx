dfba117dc7e4df84640172df5015c3a342f1e9f7
Andre Barros <andrebarros7201@gmail.com>
2024-07-11 12:49:19 +0100

    create utils folder and create calculateInvestmentGrowth.js file and remove it from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createContext, useCallback, useState } from "react";
 import Budget from "./components/Budget.jsx";
 import Prediction from "./components/Prediction.jsx";
 import Details from "./components/Details.jsx";
+import { calculateInvestmentGrowth } from "./utils/calculateInvestmentGrowth.js";
 
 export const AppContext = createContext(null);
 
@@ -66,62 +67,6 @@ function App() {
 
   const handleIncomeChange = (value) => {
     setIncome(value);
-  };
-
-  const calculateInvestmentGrowth = ({
-    initialInvestment,
-    monthlyContribution,
-    yearlyGrowth,
-    years,
-  }) => {
-    let investmentGrowth = [];
-
-    for (let i = 0; i < years; i++) {
-      // Initialize the year's data
-      investmentGrowth[i] = {
-        year: new Date().getFullYear() + i,
-        value: 0,
-        months: [
-          { month: "January", value: null },
-          { month: "February", value: null },
-          { month: "March", value: null },
-          { month: "April", value: null },
-          { month: "May", value: null },
-          { month: "June", value: null },
-          { month: "July", value: null },
-          { month: "August", value: null },
-          { month: "September", value: null },
-          { month: "October", value: null },
-          { month: "November", value: null },
-          { month: "December", value: null },
-        ],
-      };
-
-      for (let k = 0; k < 12; k++) {
-        if (i === 0 && k === 0) {
-          investmentGrowth[i].months[k].value = parseFloat(
-            (monthlyContribution + initialInvestment) *
-              (1 + yearlyGrowth / 12 / 100),
-          ).toFixed(2);
-        } else if (k === 0) {
-          investmentGrowth[i].months[k].value = parseFloat(
-            investmentGrowth[i - 1].months[11].value *
-              (1 + yearlyGrowth / 12 / 100) +
-              monthlyContribution,
-          ).toFixed(2);
-        } else {
-          investmentGrowth[i].months[k].value = parseFloat(
-            investmentGrowth[i].months[k - 1].value *
-              (1 + yearlyGrowth / 12 / 100) +
-              monthlyContribution,
-          ).toFixed(2);
-        }
-      }
-
-      investmentGrowth[i].value = investmentGrowth[i].months[11].value;
-    }
-
-    return investmentGrowth;
   };
 
   return (
