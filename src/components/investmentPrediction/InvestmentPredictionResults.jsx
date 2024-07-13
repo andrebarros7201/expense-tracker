8401ea0727ede0a7be49a9983ac47f5306c8e35a
Andre Barros <andrebarros7201@gmail.com>
2024-07-13 15:24:23 +0100

    increase space between p and change div to section

--- src/components/investmentPrediction/InvestmentPredictionResults.jsx
+++ src/components/investmentPrediction/InvestmentPredictionResults.jsx
@@ -5,53 +5,49 @@ import { AppContext } from "../../App.jsx";
 export default function InvestmentPredictionResults() {
   const { investment } = useContext(AppContext);
   return (
-    <div className={styles["investmentPredictionResult"]}>
-      {investment.growth && (
-        <div>
-          <p>
-            Initial Investment:{" "}
-            {parseFloat(investment.initialInvestment)
-              .toFixed(2)
-              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
-            €
-          </p>
-          <p>
-            Total Investment:{" "}
-            {parseFloat(investment.totalContribution)
-              .toFixed(2)
-              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
-            €
-          </p>
-          <p>
-            Growth:{" "}
-            {parseFloat(investment.totalGrowth)
-              .toFixed(2)
-              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
-            €
-          </p>
-          <p>
-            Final Year:{" "}
-            {parseFloat(investment.finalYear)
-              .toFixed(2)
-              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
-            €
-          </p>
-          <p>
-            4%:{" "}
-            {parseFloat(investment.fireFourPercent)
-              .toFixed(2)
-              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
-            €
-          </p>
-          <p>
-            FIRE Income:{" "}
-            {investment.fireMonthlyIncome
-              .toFixed(2)
-              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
-            €
-          </p>
-        </div>
-      )}
-    </div>
+    <section className={styles["investmentPredictionResult"]}>
+      <p>
+        Initial Investment:{" "}
+        {parseFloat(investment.initialInvestment)
+          .toFixed(2)
+          .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+        €
+      </p>
+      <p>
+        Total Investment:{" "}
+        {parseFloat(investment.totalContribution)
+          .toFixed(2)
+          .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+        €
+      </p>
+      <p>
+        Growth:{" "}
+        {parseFloat(investment.totalGrowth)
+          .toFixed(2)
+          .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+        €
+      </p>
+      <p>
+        Final Year:{" "}
+        {parseFloat(investment.finalYear)
+          .toFixed(2)
+          .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+        €
+      </p>
+      <p>
+        4%:{" "}
+        {parseFloat(investment.fireFourPercent)
+          .toFixed(2)
+          .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+        €
+      </p>
+      <p>
+        FIRE Income:{" "}
+        {investment.fireMonthlyIncome
+          .toFixed(2)
+          .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
+        €
+      </p>
+    </section>
   );
 }
