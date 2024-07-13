99adea504cf5aff6685a11313e258676cc6e4a49
Andre Barros <andrebarros7201@gmail.com>
2024-07-13 18:11:40 +0100

    create budgetCrud file and css

---
+++ src/components/budget/BudgetCRUD.jsx
@@ -0,0 +1,5 @@
+import styles from "../../styles/components/budget/budgetCrud.module.css";
+
+export default function BudgetCRUD() {
+  return <section className={styles["budgetCRUD"]}></section>;
+}
--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -1,5 +1,6 @@
 import BudgetDivision from "../components/budget/BudgetDivision.jsx";
 import styles from "../styles/pages/budget.module.css";
+import BudgetCRUD from "../components/budget/BudgetCRUD.jsx";
 
 export default function Budget() {
   return (
@@ -7,6 +8,7 @@ export default function Budget() {
       <h2>Monthly Budget</h2>
       <div className={styles["budget-components"]}>
         <BudgetDivision />
+        <BudgetCRUD />
       </div>
     </main>
   );
