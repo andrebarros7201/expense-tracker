3cef293806646a911e41ccb4780ea7e66d22ac45
Andre Barros <andrebarros7201@gmail.com>
2024-07-15 18:33:51 +0100

    conditional rendering when is editing

--- src/components/budget/BudgetCRUDItem.jsx
+++ src/components/budget/BudgetCRUDItem.jsx
@@ -4,13 +4,52 @@ import { useState } from "react";
 
 export default function BudgetCRUDItem({ item }) {
   const [detailsOpen, setDetailsOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [copyItem, setCopyItem] = useState(item);
+
+  function handleIsEditing() {
+    setIsEditing(true);
+  }
+
+  function handleSaveEdit() {
+    setIsEditing(false);
+  }
+
+  function handleCopyItemChange(field, value) {
+    setCopyItem({ ...item, [field]: value });
+  }
+
   return (
     <div className={styles["budgetCRUDITem"]} data-testid="budgetCRUDITem">
       <div className={styles["budgetCRUDITem-top"]}>
-        <p>{item.name}</p>
-        <p>{item.proportion}</p>
-        <button>{detailsOpen ? "Close" : "Open"} Details</button>
-        <button>Edit</button>
+        {!isEditing ? (
+          <>
+            <p>{item.name}</p>
+            <p>{item.proportion}</p>
+            <button onClick={handleIsEditing}>Edit</button>
+            <button>{detailsOpen ? "Close" : "Open"} Details</button>
+          </>
+        ) : (
+          <>
+            <input
+              type="text"
+              id={"name"}
+              value={copyItem.name}
+              onChange={(e) => handleCopyItemChange("name", e.target.value)}
+            />
+            <input
+              type="number"
+              id={"proportion"}
+              min={0}
+              value={copyItem.proportion}
+              onChange={(e) =>
+                handleCopyItemChange("proportion", e.target.value)
+              }
+            />
+            <button onClick={handleSaveEdit}>Save</button>
+            <button>{detailsOpen ? "Close" : "Open"} Details</button>
+          </>
+        )}
         <button>Delete</button>
       </div>
       {detailsOpen && (
