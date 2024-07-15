bd0ca3c6af5875e57f77497c7af68eeb5eabe8ea
Andre Barros <andrebarros7201@gmail.com>
2024-07-15 19:32:56 +0100

    change add budget item style

--- src/components/budget/BudgetAddItem.jsx
+++ src/components/budget/BudgetAddItem.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import styles from "../../styles/components/budget/budgetAddItem.module.css";
 
 export default function BudgetAddItem() {
   const [isAdding, setIsAdding] = useState(false);
   return (
     <div>
       {!isAdding ? (
-        <button>Add New Item</button>
+        <button className={styles["add-button"]}>Add New Item</button>
       ) : (
         <>
           <input type="text" />
