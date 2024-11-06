c010baf1694e0a91c2515bab4a6dc81ef6b0b912
André Barros <andrebarros7201@gmail.com>
2024-11-06 19:35:40 +0000

    improve investment results style

--- src/components/investmentPrediction/components/investmentPredictionResults/InvestmentPredictionResults.jsx
+++ src/components/investmentPrediction/components/investmentPredictionResults/InvestmentPredictionResults.jsx
@@ -1,46 +1,47 @@
+import styles from "./InvestmentPredictionResults.module.scss";
 import { useContext } from "react";
 import { AppContext } from "../../../../App.jsx";
 
 export default function InvestmentPredictionResults() {
   const { investment } = useContext(AppContext);
   return (
-    <section>
-      <p>
+    <section className={styles["result"]}>
+      <p className={styles["result__text"]}>
         Initial Investment:{" "}
         {parseFloat(investment.initialInvestment)
           .toFixed(2)
           .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
         €
       </p>
-      <p>
+      <p className={styles["result__text"]}>
         Total Investment:{" "}
         {parseFloat(investment.totalContribution)
           .toFixed(2)
           .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
         €
       </p>
-      <p>
+      <p className={styles["result__text"]}>
         Growth:{" "}
         {parseFloat(investment.totalGrowth)
           .toFixed(2)
           .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
         €
       </p>
-      <p>
+      <p className={styles["result__text"]}>
         Final Year:{" "}
         {parseFloat(investment.finalYear)
           .toFixed(2)
           .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
         €
       </p>
-      <p>
+      <p className={styles["result__text"]}>
         4%:{" "}
         {parseFloat(investment.fireFourPercent)
           .toFixed(2)
           .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
         €
       </p>
-      <p>
+      <p className={styles["result__text"]}>
         FIRE Income:{" "}
         {investment.fireMonthlyIncome
           .toFixed(2)
