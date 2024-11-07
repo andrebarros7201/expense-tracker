6ba9196294c82771cb4bb4b23e4e6f20cef8706e
André Barros <andrebarros7201@gmail.com>
2024-11-07 18:56:23 +0000

    create container file for components container

--- src/components/investmentPrediction/InvestmentPrediction.jsx
+++ src/components/investmentPrediction/InvestmentPrediction.jsx
@@ -1,11 +1,12 @@
 import styles from "./investmentPrediction.module.scss";
+import "../../styles/container.scss";
 import InvestmentPredictionTable from "./components/investmentPredictionTable/InvestmentPredictionTable.jsx";
 import InvestmentPredictionForm from "./components/investmentPredictionForm/InvestmentPredictionForm.jsx";
 import InvestmentPredictionResults from "./components/investmentPredictionResults/InvestmentPredictionResults.jsx";
 
 export default function InvestmentPrediction() {
   return (
-    <main className={styles["prediction"]}>
+    <main className={"container"}>
       <h2>Investment Prediction</h2>
       <section className={styles["prediction__container"]}>
         <InvestmentPredictionForm />
