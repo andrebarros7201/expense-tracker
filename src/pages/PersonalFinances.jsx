74138895f596a842f427b105330e5877cfcf89ed
André Barros <andrebarros7201@gmail.com>
2024-11-20 16:22:27 +0000

    add investment component to personal finances page

--- src/pages/PersonalFinances.jsx
+++ src/pages/PersonalFinances.jsx
@@ -1,10 +1,12 @@
 import "../styles/_pages.scss";
 import Budget from "../components/budget/Budget.jsx";
+import InvestmentPrediction from "../components/investmentPrediction/InvestmentPrediction.jsx";
 
 export default function PersonalFinances() {
   return (
     <main className={"page"}>
       <Budget />
+      <InvestmentPrediction />
     </main>
   );
 }
