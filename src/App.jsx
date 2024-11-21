9b5b19a32049b9e5215e38e5887d59548749bea8
André Barros <andrebarros7201@gmail.com>
2024-11-21 16:31:02 +0000

    remove theme toggle code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import "./App.scss";
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
-import { createContext, useState } from "react";
-
-export const AppContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState("light");
   return (
     <>
-      <AppContext.Provider value={{ theme, setTheme }}>
-        <Header />
-        <Outlet />
-      </AppContext.Provider>
+      <Header />
+      <Outlet />
     </>
   );
 }
