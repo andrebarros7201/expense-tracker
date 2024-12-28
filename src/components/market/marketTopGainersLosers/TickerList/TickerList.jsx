873aadd9707d2ba8112be0fc7edb79e409cd672d
André Barros <andrebarros7201@gmail.com>
2024-12-28 17:37:41 +0000

    reduce top gainers losers title margin

--- src/components/market/marketTopGainersLosers/TickerList/TickerList.jsx
+++ src/components/market/marketTopGainersLosers/TickerList/TickerList.jsx
@@ -4,7 +4,7 @@ import TickerListItem from "./TickerListItem/TickerListItem.jsx";
 export default function TickerList({ list, children }) {
   return (
     <div className={styles["list"]}>
-      <h3>{children}</h3>
+      <h3 className={styles.title}>{children}</h3>
       <div className={styles["list__header"]}>
         <p>Ticker</p>
         <p>Price</p>
