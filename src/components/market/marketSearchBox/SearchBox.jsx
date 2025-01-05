4ed0c4a848659efc48de33b9ee4aeda181ebb6aa
André Barros <andrebarros7201@gmail.com>
2025-01-05 13:38:31 +0000

    display search results

--- src/components/market/marketSearchBox/SearchBox.jsx
+++ src/components/market/marketSearchBox/SearchBox.jsx
@@ -1,52 +1,58 @@
 import styles from "./SearchBox.module.scss";
 import "../../../styles/main.scss";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MarketContext } from "../Market.jsx";
+import { Link } from "react-router-dom";
 
 export default function SearchBox() {
-  const { setTicker, ticker, setTickerData } = useContext(MarketContext);
+  const { setTicker, ticker } = useContext(MarketContext);
+  const [searchResults, setSearchResults] = useState(null);
 
   function handleChange(event) {
     setTicker(event.target.value);
   }
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    try {
-      const response = await fetch(
-        `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${import.meta.env.VITE_API_KEY}`,
-      );
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.twelvedata.com/symbol_search?symbol=${ticker}`,
+        );
+        const json = await response.json();
+        setSearchResults(json.data);
+      } catch (error) {
+        console.error(error);
       }
+    };
 
-      const data = await response.json();
-      setTickerData(data);
-    } catch (error) {
-      console.error("Failed to fetch ticker data:", error);
-    }
-  }
+    fetchData();
+  }, [ticker]);
   return (
     <form className={styles["search"]}>
-      <input
-        className={"form__input"}
-        type="text"
-        placeholder="Ticker: IBM, AAPL..."
-        required
-        min={1}
-        max={4}
-        value={ticker}
-        onChange={(e) => handleChange(e)}
-      />
-      <button
-        type={"submit"}
-        onClick={(e) => handleSubmit(e)}
-        className={"button"}
-      >
-        Search
-      </button>
+      <div className={styles["search__field"]}>
+        <input
+          className={"form__input"}
+          type="text"
+          placeholder="Ticker: IBM, AAPL..."
+          min={1}
+          max={4}
+          value={ticker}
+          onChange={(e) => handleChange(e)}
+        />
+        {searchResults ? (
+          <div className={styles["search__results"]}>
+            {searchResults.map((result, index) => (
+              <Link
+                className={styles["search__result"]}
+                key={index}
+                to={`/market/${result.symbol}`}
+              >
+                {result.symbol}
+              </Link>
+            ))}
+          </div>
+        ) : null}
+      </div>
     </form>
   );
 }
