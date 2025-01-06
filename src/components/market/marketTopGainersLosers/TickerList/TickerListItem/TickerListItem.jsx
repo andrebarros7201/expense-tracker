0e213084e0c130b87f09a3af52a639327dfb9780
André Barros <andrebarros7201@gmail.com>
2025-01-06 11:03:11 +0000

    change gainers and losers item to div instead of Link

--- src/components/market/marketTopGainersLosers/TickerList/TickerListItem/TickerListItem.jsx
+++ src/components/market/marketTopGainersLosers/TickerList/TickerListItem/TickerListItem.jsx
@@ -1,17 +1,16 @@
 import styles from "./TickerListItem.module.scss";
 import "../../../../../styles/main.scss";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
 export default function TickerList({ item }) {
   return (
-    <Link to={`/market/${item.ticker}`} className={styles["item"]}>
+    <div className={styles["item"]}>
       <p className={styles["item__name"]}>{item.ticker}</p>
       <p>{Number(item.price).toFixed(3)} $</p>
       <p>{Number(item.change_amount).toFixed(2)} $</p>
       <p>{parseFloat(item.change_percentage).toFixed(2)} %</p>
       <p>{item.volume}</p>
-    </Link>
+    </div>
   );
 }
 
