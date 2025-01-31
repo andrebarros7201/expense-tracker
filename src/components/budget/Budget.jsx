486d25da04f743d87d72f278ef0dcf0d8ae8cdb4
André Barros <andrebarros7201@gmail.com>
2025-01-31 12:05:04 +0000

    save budget on localStorage

--- src/components/budget/Budget.jsx
+++ src/components/budget/Budget.jsx
@@ -1,10 +1,20 @@
-import "../../styles/main.scss";
-import styles from "./budget.module.scss";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
 import BudgetDistribution from "./components/budgetDistribution/BudgetDistribution.jsx";
 import BudgetAddItemForm from "./components/budgetAddItem/BudgetAddItemForm.jsx";
 import BudgetIncome from "./components/budgetIncome/BudgetIncome.jsx";
 
+import "../../styles/main.scss";
+import styles from "./budget.module.scss";
+
 export default function Budget() {
+  const budget = useSelector((state) => state.budget);
+
+  useEffect(() => {
+    localStorage.setItem("budget", JSON.stringify(budget));
+  }, [budget]);
+
   return (
     <main className={"container"}>
       <h3>Monthly Budget</h3>
